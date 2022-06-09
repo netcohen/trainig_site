e7d37ca792de0cf7c4558fc27d8db7902e84fceb
netcohen <netcohen@101.org.il>
2022-06-09 17:50:26 +0300

    עיצוב לוח שנה + הוספת סטייל מיוטיוב

--- calendar.js
+++ calendar.js
@@ -1,4 +1,4 @@
-const newCalendar = document.getElementById("calendar");
+const Calendar = document.getElementById("calendar");
 const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
 let nav = 0;
@@ -17,8 +17,32 @@ function load() {
 
     const daysInMonth = new Date(year, month +1, 0).getDate();
 const firstDayOfTheMonth = new Date(year, month, 1)
-const dateToString = firstDayOfTheMonth.toLocaleDateString('en-us');
-    console.log(dateToString)
+const dateString = firstDayOfTheMonth.toLocaleDateString('en-us', {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+});
+
+const paddingDays = weekDays.indexOf(dateString.split(', ')[0]);    
+
+for (let i=1; i<=paddingDays+daysInMonth; i++) {
+    const daySquare = document.createElement('div');
+    daySquare.classList.add('day');
+
+    if(i>paddingDays) {
+        daySquare.innerText = i - paddingDays;
+
+        daySquare.addEventListener('click',() => console.log('click'));
+
+    } else {
+        daySquare.classList.add('padding')
+    }
+
+Calendar.appendChild(daySquare);    
+
+}
+    console.log(paddingDays)
 
 };
 
