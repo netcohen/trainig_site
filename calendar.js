1327f52386e5ab36781a463a5a417847823f4e6f
netcohen <netcohen@101.org.il>
2022-06-10 13:20:36 +0300

    סיום לוח שנה בסיסי

--- calendar.js
+++ calendar.js
@@ -1,6 +1,9 @@
 const Calendar = document.getElementById("calendar");
 const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-
+const newEventModel = document.getElementById('newEventModal');
+const deleteEventModel = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modelBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput')
 
 let nav = 0;
 let clicked = null;
@@ -8,9 +11,29 @@ let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('e
 
 
 
+function openModel(date) {
+clicked = date;
+
+const eventForDay = events.find(e=> e.date === clicked)
+
+if (eventForDay) {
+    document.getElementById('eventText').innerText = eventForDay.title;
+    deleteEventModel.style.display = 'block'
+} else {
+    newEventModel.style.display = 'block';
+}
+
+backDrop.style.display = 'block'
+}
+
+
+
 function load() {
 
     const dt = new Date();
+    if(nav !== 0) {
+        dt.setMonth(new Date().getMonth() + nav)
+    }
 
     const day = dt.getDate();
     const month = dt.getMonth();
@@ -27,15 +50,29 @@ const dateString = firstDayOfTheMonth.toLocaleDateString('en-us', {
 
 const paddingDays = weekDays.indexOf(dateString.split(', ')[0]);   
 document.getElementById('monthDisplay').innerText = `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+Calendar.innerHTML= '';
 
 for (let i=1; i<=paddingDays+daysInMonth; i++) {
     const daySquare = document.createElement('div');
     daySquare.classList.add('day');
-
+    const dayString = `${i-paddingDays}/${month + 1}/${year}` 
     if(i>paddingDays) {
         daySquare.innerText = i - paddingDays;
+    
+        const eventForDay = events.find(e=> e.date === dayString)
 
-        daySquare.addEventListener('click',() => console.log('click'));
+        if( i - paddingDays === day && nav === 0) {
+            daySquare.id = "currentDay"
+        }
+
+        if(eventForDay) {
+            const eventDiv = document.createElement('div');
+            eventDiv.classList.add('event');
+            eventDiv.innerText = eventForDay.title;
+            daySquare.appendChild(eventDiv)
+        }
+
+        daySquare.addEventListener('click',() => openModel(`${i-paddingDays}/${month + 1}/${year}`));
 
     } else {
         daySquare.classList.add('padding')
@@ -44,10 +81,57 @@ for (let i=1; i<=paddingDays+daysInMonth; i++) {
 Calendar.appendChild(daySquare);    
 
 }
-    console.log(paddingDays)
 
 };
 
+function closeModal() {
+    eventTitleInput.classList.remove('error');
+    newEventModel.style.display = 'none';
+    deleteEventModel.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.value = '';
+    clicked = 'null';
+    load();
+}
+
+function saveEvent() {
+    if (eventTitleInput.value) {
+        eventTitleInput.classList.remove('error');
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value,
+        })
+
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+    }else{
+        eventTitleInput.classList.add('error');
+    }
+}
+
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal()
+}
+
+function initButton() {
+    document.getElementById('nextButton').addEventListener('click', () => {
+        nav++
+        load();
+    });
+    document.getElementById('backButton').addEventListener('click', () => {
+        nav--
+        load();
+    });
+
+    document.getElementById('saveButton').addEventListener('click', saveEvent);
+    document.getElementById('cancelButton').addEventListener('click', closeModal);
+    document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+    document.getElementById('closeButton').addEventListener('click', closeModal);
+}
+
+initButton();
 load();
 
 
